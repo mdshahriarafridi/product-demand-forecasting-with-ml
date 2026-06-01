8f89a4d270cc662fc1bc2786425367c6fa66694a
Afridi <shahriarafridi66@gmail.com>
2026-06-02 00:57:28 +0600

    new commit

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ from fastapi.responses import FileResponse
 from pydantic import BaseModel
 import pickle
 import pandas as pd
-import uvicorn
 
 app = FastAPI()
 
@@ -51,7 +50,3 @@ def predict(data: PredictRequest):
 
     prediction = model.predict(input_data)[0]
     return {"prediction": int(prediction)}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
